d3f1d92d486f09816c138eacf98b06096450a476
Shawn Cockburn <shawn@Administrators-iMac.local>
2020-11-19 09:48:43 +0000

    First commit, added basic reverse proxy functionality

---
+++ proxy.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const https = require('https');
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
+
+const nodeProxy = config => {
+    const {
+        routes,
+        /*
+        routes array:
+        [{host:"example.com",target:"http://localhost:3000"},{host:"api.example.com",target:"http://localhost:4000"}]
+        */
+        forceSSL = false,
+        /*
+       forceSSL:
+       forces all http requests to redirect to https requests.
+       default = false.
+       (HIGHLY RECOMMEND THIS IS SET TO TRUE)
+       */
+        sslConfig
+        /*
+      sslConfig:
+      {
+          key: privateKey,
+          cert: certificate,
+          ca: intermediateCertificate
+      }
+      */
+    } = config
+
+    if (routes === undefined || routes.length == 0) throw "Routes must be configurated."
+    if (sslConfig === undefined) throw "SSL must be configurated."
+
+
+    // force ssl
+    if (forceSSL) {
+        app.use((req, res, next) => {
+            if (req.connection.encrypted) return next();
+            res.redirect(301, 'https://' + req.headers.host + req.url);
+        });
+    }
+
+    // proxies
+    app.all("*", function (req, res) {
+        const host = req.headers.host;
+        const requestedRoute = routes.find(route => route.host === host);
+        if (!requestedRoute) throw `${host} not found in routes!`;
+        apiProxy.web(req, res, { target: requestedRoute.target });
+    });
+
+    const sslServer = () => {
+        // Starting http server
+        const httpServer = http.createServer(app);
+        const httpport = 80;
+        httpServer.listen(httpport, () => {
+            console.log('HTTP Server running');
+        });
+
+        // Starting https server
+        const httpsServer = https.createServer(sslConfig, app);
+        const httpsport = 443;
+        httpsServer.listen(httpsport, () => {
+            console.log('HTTPS Server running');
+        });
+    };
+
+    sslServer();
+}
+
+module.exports = nodeProxy;
