a7b89928b59eb866b0961fdbb6176f7d2eb5feb1
ShawnCockburn <shawncockburn@hotmail.com>
2021-09-05 17:47:59 +0100

    !update: added ts

--- proxy.ts
+++ proxy.ts
@@ -1,52 +1,52 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
+import express = require('express');
 const app = express();
-const http = require("http");
-const https = require("https");
-const httpProxy = require("http-proxy");
+import http = require('http');
+import https = require('https');
+import httpProxy = require('http-proxy');
 const apiProxy = httpProxy.createProxyServer();
+
 const nodeProxy = config => {
-    const { routes, 
-    /*
-    routes array:
-    [{host:"example.com",target:"http://localhost:3000"},{host:"api.example.com",target:"http://localhost:4000"}]
-    */
-    forceSSL = false, 
-    /*
-   forceSSL:
-   forces all http requests to redirect to https requests.
-   default = false.
-   (HIGHLY RECOMMEND THIS IS SET TO TRUE)
-   */
-    sslConfig
-    /*
-  sslConfig:
-  {
-      key: privateKey,
-      cert: certificate,
-      ca: intermediateCertificate
-  }
-  */
-     } = config;
-    if (routes === undefined || routes.length == 0)
-        throw "Routes must be configurated.";
-    if (sslConfig === undefined)
-        throw "SSL must be configurated.";
+    const {
+        routes,
+        /*
+        routes array:
+        [{host:"example.com",target:"http://localhost:3000"},{host:"api.example.com",target:"http://localhost:4000"}]
+        */
+        forceSSL = false,
+        /*
+       forceSSL:
+       forces all http requests to redirect to https requests.
+       default = false.
+       (HIGHLY RECOMMEND THIS IS SET TO TRUE)
+       */
+        sslConfig
+        /*
+      sslConfig:
+      {
+          key: privateKey,
+          cert: certificate,
+          ca: intermediateCertificate
+      }
+      */
+    } = config
+
+    if (routes === undefined || routes.length == 0) throw "Routes must be configurated."
+    if (sslConfig === undefined) throw "SSL must be configurated."
+
+
     // force ssl
     if (forceSSL) {
         app.use((req, res, next) => {
-            if (req.connection.encrypted)
-                return next();
+            if (req.connection.encrypted) return next();
             res.redirect(301, 'https://' + req.headers.host + req.url);
         });
     }
+
     // proxies
     app.all("*", function (req, res) {
         const host = req.headers.host;
         const requestedRoute = routes.find(route => route.host === host || ("www." + route.host) === host);
-        if (!requestedRoute)
-            return res.statusCode(404);
+        if (!requestedRoute) return res.statusCode(404);
         try {
             apiProxy.web(req, res, { target: requestedRoute.target });
             apiProxy.on('error', (err, req, res) => {
@@ -55,25 +55,28 @@ const nodeProxy = config => {
                 });
                 res.end('Something went wrong.');
             });
-        }
-        catch (error) {
+        } catch (error) {
             res.statusCode(500);
         }
+
+
     });
+
     return { app: app, sslConfig: sslConfig };
-};
+}
+
 const Server = (serverObject) => {
     const { app, sslConfig } = serverObject;
-    if (app === undefined)
-        throw "Express Proxy Server Required!";
-    if (sslConfig === undefined)
-        throw "SSL must be configurated.";
+    if (app === undefined) throw "Express Proxy Server Required!";
+    if (sslConfig === undefined) throw "SSL must be configurated."
+
     // Starting http server
     const httpServer = http.createServer(app);
     const httpport = 80;
     httpServer.listen(httpport, () => {
         console.log('HTTP Server running');
     });
+
     // Starting https server
     const httpsServer = https.createServer(sslConfig, app);
     const httpsport = 443;
@@ -81,5 +84,6 @@ const Server = (serverObject) => {
         console.log('HTTPS Server running');
     });
 };
+
 module.exports = nodeProxy;
 module.exports.Server = Server;
