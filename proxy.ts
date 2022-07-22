da4ef2772745e51d37527ad3315f41f87119bbed
Shawn Cockburn <shawncockburn@hotmail.com>
2022-07-22 23:31:55 +0100

    fix: header sending too soon bug

--- proxy.ts
+++ proxy.ts
@@ -30,15 +30,17 @@ const nodeProxy = config => {
       */
     } = config
 
-    if (routes === undefined || routes.length == 0) throw "Routes must be configurated."
-    if (sslConfig === undefined) throw "SSL must be configurated."
+    if (routes === undefined || routes.length == 0) throw "Routes must be configured."
+    if (sslConfig === undefined) throw "SSL must be configured."
 
 
     // force ssl
     if (forceSSL) {
         app.use((req, res, next) => {
+            //@ts-ignore
             if (req.connection.encrypted) return next();
             res.redirect(301, 'https://' + req.headers.host + req.url);
+            return
         });
     }
 
@@ -46,17 +48,16 @@ const nodeProxy = config => {
     app.all("*", function (req, res) {
         const host = req.headers.host;
         const requestedRoute = routes.find(route => route.host === host || ("www." + route.host) === host);
-        if (!requestedRoute) return res.statusCode(404);
+        if (!requestedRoute) return res.status(404);
         try {
             apiProxy.web(req, res, { target: requestedRoute.target });
-            apiProxy.on('error', (err, req, res) => {
-                res.writeHead(500, {
-                    'Content-Type': 'text/plain'
-                });
-                res.end('Something went wrong.');
+            apiProxy.on('error', (err) => {
+                res.status(500).send('Something went wrong.');
+                return
             });
         } catch (error) {
-            res.statusCode(500);
+            res.status(500);
+            return
         }
 
 
